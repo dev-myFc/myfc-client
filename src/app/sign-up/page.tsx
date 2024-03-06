a9bfaeaf7a99070b1ec47f157a33b027b82c9675
SungminPark1997 <72503832+SungminPark1997@users.noreply.github.com>
2024-03-06 23:12:07 +0900

    Add sign-up page style

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,9 +1,36 @@
+"use client";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "회원가입",
-};
-
+import styles from "../../styles/sign-up.module.css";
+import btnStyle from "../../styles/button.module.css";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 export default function SignUp() {
-  return <div>여기는 회원가입창</div>;
+  const { register, handleSubmit } = useForm();
+  const router = useRouter();
+  const onSubmit = () => {
+    alert("회원가입이 완료되었습니다");
+    router.push("/");
+  };
+  return (
+    <div className={styles.wrapper}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <input
+          type="tel"
+          placeholder="전화번호를 입력하세요"
+          className={styles.input}
+        ></input>
+        <input
+          type="password"
+          placeholder="비밀번호를 입력하세요"
+          className={styles.input}
+        ></input>
+        <input
+          type="submit"
+          value={"회원가입하기"}
+          className={btnStyle.button}
+        />
+      </form>
+    </div>
+  );
 }
--- src/component/Button.tsx
+++
@@ -1,4 +0,0 @@
-import styles from "../styles/button.module.css";
-export default function Button() {
-  return <div className={styles.button}></div>;
-}
